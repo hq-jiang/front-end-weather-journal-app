aad8ad8d8c922b1fb5584dc9b15f3ac6bcbae8b4
Hanqiu Jiang <hanq.jiang@gmail.com>
2020-04-03 01:11:00 +0200

    feat: Get zip code and query weather data

--- website/app.js
+++ website/app.js
@@ -4,10 +4,15 @@
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+
+baseUrl = "http://api.openweathermap.org/data/2.5/weather";
+credentials = "APPID=cb23854f95face62703a8ce74dc109f1";
+
 let button = document.querySelector("#generate");
-button.addEventListener("click", getRequest);
-button.addEventListener("click", postRequest);
-button.addEventListener("click", getWeatherData);
+// button.addEventListener("click", getRequest);
+// button.addEventListener("click", postRequest);
+button.addEventListener("click", getWeatherData.bind(null, baseUrl, credentials));
+// button.addEventListener("click", getZipCode);
 
 async function getRequest() {
   const response = await fetch("/test-get", {
@@ -31,8 +36,16 @@ async function postRequest() {
   console.log(responseData);
 }
 
-async function getWeatherData() {
-  const response = await fetch("http://api.openweathermap.org/data/2.5/weather?zip=70191,de&units=metric&APPID=cb23854f95face62703a8ce74dc109f1");
-  const responseData = await response.json();
-  console.log("Current temperature in Stuttgart is", responseData["main"]["temp"], "degrees");
+async function getWeatherData(base, credentials) {
+  const zipCode = getZipCode();
+  const country = "de"; // Germany
+  fullUrl = `${base}?zip=${zipCode},${country}&units=metric&${credentials}`;
+  const response = await fetch(fullUrl);
+  const res = await response.json();
+  console.log(`Current temperature in ${res["name"]} is ${res["main"]["temp"]}°C`);
+}
+
+function getZipCode() {
+  const zipCode = document.querySelector("#zip").value;
+  return zipCode;
 }

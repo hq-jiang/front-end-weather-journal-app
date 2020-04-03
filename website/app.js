e7ed10dae957d686f14fa2fcd5467d5c98ac4eab
Hanqiu Jiang <hanq.jiang@gmail.com>
2020-04-03 13:45:33 +0200

    feat: Improve error handling

--- website/app.js
+++ website/app.js
@@ -2,17 +2,38 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let NEWDATE = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 
-baseUrl = "http://api.openweathermap.org/data/2.5/weather";
-credentials = "APPID=cb23854f95face62703a8ce74dc109f1";
+BASEURL = "http://api.openweathermap.org/data/2.5/weather";
+CREDENTIALS = "APPID=cb23854f95face62703a8ce74dc109f1";
 
 let button = document.querySelector("#generate");
 // button.addEventListener("click", getRequest);
 // button.addEventListener("click", postRequest);
-button.addEventListener("click", getWeatherData.bind(null, baseUrl, credentials));
-// button.addEventListener("click", getZipCode);
+// button.addEventListener("click", getWeatherData.bind(null, baseUrl, credentials));
+// button.addEventListener("click", getFeelsText);
+button.addEventListener("click", postData);
+
+async function postData () {
+  try {
+    const projectData = await collectData();
+    console.log(projectData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function collectData() {
+  let projectData = {};
+
+  // Collect project data
+  projectData.weather = await getWeatherData(BASEURL, CREDENTIALS);
+  projectData.date = NEWDATE;
+  projectData.feels = getFeelsText();
+  return projectData;
+
+}
 
 async function getRequest() {
   const response = await fetch("/test-get", {
@@ -42,10 +63,21 @@ async function getWeatherData(base, credentials) {
   fullUrl = `${base}?zip=${zipCode},${country}&units=metric&${credentials}`;
   const response = await fetch(fullUrl);
   const res = await response.json();
-  console.log(`Current temperature in ${res["name"]} is ${res["main"]["temp"]}°C`);
+  if (res.cod == 400 || res.cod == 404) {
+    throw "getWeatherData: "+res.message;
+  }
+  temp = res.main.temp;
+  city = res.name;
+  return `Current temperature in ${city} is ${temp}°C`;
 }
 
 function getZipCode() {
   const zipCode = document.querySelector("#zip").value;
   return zipCode;
+}
+
+function getFeelsText() {
+  const feelsText = document.querySelector("#feelings").value;
+  console.log(feelsText);
+  return feelsText;
 }
